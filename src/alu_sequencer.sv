f6e0da64d15c2c7100e78fe08d1dcee13db44dcf
NISHANTH GOWDA CJ <nishanthjgowda9740@gmail.com>
2025-08-22 10:11:57 +0530

    alu_sequencer.sv

---
+++ src/alu_sequencer.sv
@@ -0,0 +1,17 @@
+class alu_sequencer extends uvm_sequencer #(alu_sequence_item);
+  `uvm_component_utils(alu_sequencer)
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+endclass
+
+[nishanthgowda@feserver uvm_project]$ cat alu_sequencer.sv
+
+class alu_sequencer extends uvm_sequencer #(alu_sequence_item);
+  `uvm_component_utils(alu_sequencer)
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+endclass
